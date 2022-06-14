f2b2e58da6195a3197f2ce25004c65a98878aa30
hungw7 <gaughegomth01@gmail.com>
2022-06-14 22:54:20 +0700

    update task deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,12 @@
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-etherscan";
 import "ethereum-waffle";
-import { solidity } from "ethereum-waffle";
-import chai from "chai";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 import "hardhat-gas-reporter";
-import "./scripts/tasks";
 import dotenv from "dotenv";
-import path from "path";
+import "./scripts/tasks";
 
 dotenv.config();
-//override with node env
-dotenv.config({ path: path.join(__dirname, `.env.${process.env.NODE_ENV}`) });
-chai.use(solidity);
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -23,26 +17,35 @@ const { DEPLOYER_PRIVATE_KEY, PROJECT_ID, ETHERSCAN_API_KEY } = process.env;
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-	solidity: "0.8.4",
-	defaultNetwork: "rinkeby",
-	networks: {
-		hardhat: {
-			chainId: 1337
-		},
-		rinkeby: {
-			url: `https://arbitrum-rinkeby.infura.io/v3/${PROJECT_ID}`,
-			accounts: [`0x${DEPLOYER_PRIVATE_KEY}`]
-		}
-	},
-	etherscan: {
-		apiKey: ETHERSCAN_API_KEY
-	},
-	gasReporter: {
-		enabled: true,
-		showTimeSpent: true,
-		showGasSpent: true,
-		token: "ETH",
-		currency: "USD",
-		coinmarketcap: "2bb80343-6daa-4193-8f2b-732eba7a2e56"
-	}
+  solidity: {
+    version: "0.8.5",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${PROJECT_ID}`,
+      accounts: [`${DEPLOYER_PRIVATE_KEY}`],
+      gasPrice: 225000000000
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    showTimeSpent: true,
+    showGasSpent: true,
+    token: "ETH",
+    currency: "USD",
+    coinmarketcap: "2bb80343-6daa-4193-8f2b-732eba7a2e56"
+  }
 };
--- scripts/helper.ts
+++ scripts/helper.ts
@@ -1,26 +1,30 @@
 import { ethers } from "ethers";
 
 function getEnvVariable(key: string, defaultReturn?: string): string {
-	if (process.env[key]) {
-		return process.env[key]!;
-	}
+  if (process.env[key]) {
+    return process.env[key]!;
+  }
 
-	if (!defaultReturn) {
-		throw new Error(`${key} is not provided or default`);
-	}
+  if (!defaultReturn) {
+    throw new Error(`${key} is not provided or default`);
+  }
 
-	return defaultReturn;
+  return defaultReturn;
 }
 
 export function getProvider(): ethers.providers.BaseProvider {
-	return ethers.getDefaultProvider(getEnvVariable("NETWORK", "rinkeby"), {
-		alchemy: getEnvVariable("PROJECT_ID")
-	});
+  return ethers.getDefaultProvider(getEnvVariable("NETWORK", "rinkeby"), {
+    etherscan: getEnvVariable("ETHERSCAN_API_KEY"),
+    infura: {
+      projectId: getEnvVariable("PROJECT_ID"),
+      projectSecret: getEnvVariable("PROJECT_SECRET")
+    }
+  });
 }
 
 export function getAccount() {
-	return new ethers.Wallet(
-		getEnvVariable("DEPLOYER_PRIVATE_KEY"),
-		getProvider()
-	);
+  return new ethers.Wallet(
+    getEnvVariable("DEPLOYER_PRIVATE_KEY"),
+    getProvider()
+  );
 }
